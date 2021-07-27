9a4a50def2fbcbdaa1d7d6cf4a970e613fd62d7b
yuanwenhui <yuanwenhui93@163.com>
2021-07-27 13:40:14 +0800

    feat: webpack 资源管理

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import _ from 'lodash';
+import './style.css'
+import icon from './icon.jpeg'
+import printMe from "./print";
 function component() {
   const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], '');
+  const btn = document.createElement('button');
+  btn.innerHTML = 'click me and check the console'
+  btn.onclick = printMe
+  const newIcon = new Image();
+  newIcon.src = icon;
+  element.appendChild(newIcon)
   return element
 }
 document.body.appendChild(component())
