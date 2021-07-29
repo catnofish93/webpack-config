127c41c846fdcba99eabfcba1bb038738ba8c2f0
yuanwenhui <yuanwenhui93@163.com>
2021-07-29 10:53:52 +0800

    feat: treeshaking, 环境区分

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import _ from 'lodash';
 import './style.css'
 import icon from './icon.jpeg'
 import printMe from "./print";
+import { cube } from "./math";
+
 function component() {
   const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], '');
   const btn = document.createElement('button');
-  btn.innerHTML = 'click me and check the console'
+  btn.innerHTML = 'hello webpack, 6 cubed is equal to ' + cube(6)
   btn.onclick = printMe
   element.appendChild(btn)
   const newIcon = new Image();
--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,26 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = {
-  // entry: {
-  //   index: {
-  //     import:'./src/index.js',
-  //     dependOn: 'shared',
-  //   },
-  //   print: {
-  //     import: './src/print.js',
-  //     dependOn: 'shared',
-  //   },
-  //   shared: 'lodash'
-  // },
-  entry: './src/codeSplit.js',
+  entry: {
+    index: {
+      import:'./src/index.js',
+      dependOn: 'shared',
+    },
+    // print: {
+    //   import: './src/print.js',
+    //   dependOn: 'shared',
+    // },
+    shared: 'lodash'
+  },
+  // entry: './src/library.js',
   output: {
-    filename: "[name].[contenthash].js",
+    // filename: "bundle.js",
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
+    // library: {
+    //   name: 'webpackNumbers',
+    //   type: 'umd'
+    // }
   },
   module: {
     rules: [
@@ -35,16 +39,32 @@ module.exports = {
       new HtmlWebpackPlugin({
         title: '管理输出'
       }),
-      new BundleAnalyzerPlugin()
+      // new BundleAnalyzerPlugin()
   ],
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist'
   },
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      // chunks: 'all',
+  externals: {
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: '_'
     }
+  },
+  mode: "development",
+  optimization: {
+    // runtimeChunk: 'single',
+    // splitChunks: {
+    //   cacheGroups: {
+    //     vendor: {
+    //       test: /[\\/node_modules[\\/]/,
+    //       name: 'vendor',
+    //       chunks: "all"
+    //     }
+    //   }
+    // }
+    usedExports: true
   },
 }
